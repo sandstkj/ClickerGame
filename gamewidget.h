04326d4bec52193b3d1907c3bb987fbd8179861f
Kyle <sandstkj@miamioh.edu>
2015-07-15 16:13:58 -0400

    Create gamewidget.h

---
+++ gamewidget.h
@@ -0,0 +1,37 @@
+#ifndef GAMEWIDGET_H
+#define GAMEWIDGET_H
+
+#include <QWidget>
+#include <QPainter>
+#include <iostream>
+#include <QLabel>
+#include <QHBoxLayout>
+#include 
+
+
+class gameWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    gameWidget(QWidget *parent, QSize tempSize);
+
+signals:
+
+public slots:
+
+protected:
+    void paintEvent(QPaintEvent *e);
+
+protected slots:
+    void increaseCount();
+
+private:
+    QPainter painter;
+    QHBoxLayout *layout;
+
+    int count;
+
+
+};
+
+#endif // GAMEWIDGET_H
