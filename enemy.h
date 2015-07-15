b3db5a767de03bde29fbc876440e711da6565fb3
Kyle <sandstkj@miamioh.edu>
2015-07-15 16:21:44 -0400

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,15 +1,20 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 #include <QPixmap>
+#include <qstring>
 
 class Enemy
 {
 public:
-    Enemy();
+    Enemy(QString name, int health);
 private:
+    QString name;
     QPixmap image;
+
+    int health;
+
 protected:
-    
+
 };
 
 #endif // ENEMY_H
