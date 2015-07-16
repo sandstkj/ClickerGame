fd4bb289f0497935197f1ab8d57b13b1c1f70072
Kyle <sandstkj@miamioh.edu>
2015-07-16 17:42:13 -0400

    Began Method Implementation
    Added paintEvent, damage, and getHealth methods to the class.

--- enemy.cpp
+++ enemy.cpp
@@ -1,7 +1,31 @@
 #include "enemy.h"
+#include <iostream>
 
-Enemy::Enemy(QString name, int health)
+Enemy::Enemy(QWidget *parent, QString name, QString enemyImage, int health) :
+    QWidget(parent)
 {
+    painter = new QPainter();
+    dir = enemyImage;
     this->name = name;
+    image = new QPixmap(enemyImage);
     this->health = health;
+    resize(300,300);
+}
+
+void
+Enemy::paintEvent(QPaintEvent *e) {
+
+    QPainter painter(this);
+    painter.drawPixmap(0, 0, image->width(), image->height(), dir);
+    std::cout << "Health: " << health << std::endl;
+}
+
+void
+Enemy::damage(int hit){
+    health -= hit;
+}
+
+int
+Enemy::getHealth(){
+    return health;
 }
