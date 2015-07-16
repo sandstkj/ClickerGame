ff75e338f9c9da3d577af7b5ad59389f42e25b47
Kyle <sandstkj@miamioh.edu>
2015-07-16 17:44:54 -0400

    Added Functionality
    Enhanced the increaseCount method, added an enemy instance "hostile" to the widget, and connected clicking functionality to the program.

--- gamewidget.cpp
+++ gamewidget.cpp
@@ -1,32 +1,67 @@
 #include "gamewidget.h"
 
-
-gameWidget::gameWidget(QWidget *parent, QSize tempSize) :
-    QWidget(parent), count(0)
+GameWidget::GameWidget(QWidget *parent, QSize tempSize) :
+    QWidget(parent), state(0), count(0)
 {
 
     resize(tempSize);
 
-    Button *face = new Button(QPixmap("/home/kyle/add.png"), this);
-    
-//    layout = new QHBoxLayout();
-//    QLabel* label = new QLabel(this, 0);
-//    label->setText("Hello, World!");
-//    connect(label, SIGNAL(), this, SLOT(increaseCount()));
+    //Connecting the widget to mouse events
+    connect(this, SIGNAL(release()), this, SLOT(increaseCount()));
 
-//    layout->addWidget(label);
-//    setLayout(layout);
+    //Creating enemy "Hostile"
+    hostile = new Enemy(this, "Bob", "/home/kyle/Pictures/Memes/JoeHead.png", 100);
+    hostile->lower();
+
+    //Sorting layout for health, name, etc.
+    layout = new QHBoxLayout();
+    label = new QLabel(this, 0);
+    updateHealth();
+    label->setText("Health: 100");
+    //layout->addWidget(hostile);
+    setLayout(layout);
 }
 
 void
-gameWidget::paintEvent(QPaintEvent *e) {
-
+GameWidget::paintEvent(QPaintEvent *e) {
     QPainter painter(this);
-    painter.drawEllipse(10,10,1000,1000);
+    painter.setPen(Qt::green);
+    //painter.drawEllipse(10,10,1000,1000);
 }
 
 void
-gameWidget::increaseCount(){
+GameWidget::increaseCount(){
     count++;
     std::cout << count << std::endl;
+    hostile->damage(1);
+    label->setText("Health: " + (QString)(hostile->getHealth()));
 }
+
+void
+GameWidget::mousePressEvent(QMouseEvent* e)
+{
+         state = 1;
+         repaint();
+         emit pressed();
+}
+
+void
+GameWidget::mouseReleaseEvent(QMouseEvent* e)
+{
+         state = 0;
+         repaint();
+         emit release();
+         emit clicked();
+}
+
+//void
+//GameWidget::release(){
+
+
+//}
+
+//void
+//GameWidget::clicked(){
+
+
+//}
